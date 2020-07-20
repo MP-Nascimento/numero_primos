58fc070979749d7c08515c6b6a96b6019fca6eda
MP-Nascimento <mig.nascipereira@gmail.com>
2020-07-20 19:36:37 -0300

    numero primos

---
+++ numeros_primos/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace numeros_primos
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int max = 100;
+
+            for(int i = 0; i < max; i++)
+            {
+                if (i == 2) Console.WriteLine(i);
+
+                else
+                {
+                    if (i % 2 == 0) continue;
+
+                    else
+                    {
+                        bool ePrimo = true;
+
+                        for (int j = 3; j < i / 2; j++)
+
+                        {
+                            if (i % j == 0)
+                            {
+                                ePrimo = false;
+                                break;
+                            }
+
+                        }
+
+                        if (ePrimo) Console.WriteLine(i);
+                    }   
+
+                }
+                
+            }
+
+
+        }
+    }
+}
